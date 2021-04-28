281aee6c068af1cc5c10c4acdc9115a68544ec67
Matheus Ramalho <theuramalho@gmail.com>
2021-04-27 21:01:26 -0300

    page podcast

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
+import Link from 'next/link';
+
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
@@ -13,7 +16,6 @@ type Episode = {
 	title: string;
 	members: string;
 	thumbnail: string;
-	description: string;
 	url: string;
 	duration: string;
 	durationAsString: string;
@@ -46,7 +48,9 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 									/>
 
 									<div className={styles.episodeDetails}>
-										<a href=""> {episode.title} </a>
+										<Link href={`/episodes/${episode.id}`}>
+											<a> {episode.title} </a>
+										</Link>
 										<p> {episode.members} </p>
 										<span> {episode.publishedAt} </span>
 										<span> {episode.durationAsString} </span>
@@ -67,12 +71,14 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 
 				<table cellSpacing={0}>
 					<thead>
-						<th></th>
-						<th> Podcast </th>
-						<th> Integrantes </th>
-						<th> Data </th>
-						<th> Duração </th>
-						<th></th>
+						<tr>
+							<th></th>
+							<th> Podcast </th>
+							<th> Integrantes </th>
+							<th> Data </th>
+							<th> Duração </th>
+							<th></th>
+						</tr>
 					</thead>
 
 					<tbody>
@@ -89,7 +95,11 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 										/>
 									</td>
 
-									<td><a href=""> { episode.title } </a></td>
+									<td>
+										<Link href={`/episodes/${episode.id}`}>
+											<a> {episode.title} </a>
+										</Link>
+									</td>
 									<td> { episode.members } </td>
 									<td style={{ width: 100 }}> { episode.publishedAt } </td>
 									<td> { episode.durationAsString } </td>
@@ -126,7 +136,6 @@ export const getStaticProps: GetStaticProps = async () => {
 			thumbnail: episode.thumbnail,
 			members: episode.members,
 			publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }), // parseISO transforma string em data do javascript.
-			description: episode.description,
 			duration: Number(episode.file.duration),
 			durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
 			url: episode.file.url,
